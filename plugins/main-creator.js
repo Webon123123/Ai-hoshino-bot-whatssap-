f296d09f08ab12da3d7d628cd569de8cb7b5b5dc
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2024-09-28 00:07:38 -0500

    Delete plugins directoryk

--- plugins/main-creator.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Masha⁩;;\nFN:Masha\nORG:Masha⁩\nTITLE:\nitem1.TEL;waid=595976230899:5218261000681\nitem1.X-ABLabel:Masha⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Masha⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'Masha⁩', contacts: [{ vcard }] }}, {quoted: m})
-}
-handler.help = ['owner']
-handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño'] 
-
-export default handler
