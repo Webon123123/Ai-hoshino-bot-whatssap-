3efbf7e3761a61c7253944502d5be85da6739371
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2024-09-27 22:33:15 -0500

    Update main-creator.js

--- plugins/main-creator.js
+++ plugins/main-creator.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, usedPrefix, isOwner }) => {
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;おDanịel.xyz⁩;;\nFN:おDanịel.xyz⁩\nORG:おDanịel.xyz⁩\nTITLE:\nitem1.TEL;waid=5218261000681:5218261000681\nitem1.X-ABLabel:おDanịel.xyz⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:おDanịel.xyz⁩\nEND:VCARD`
-await conn.sendMessage(m.chat, { contacts: { displayName: 'おDanịel.xyz⁩', contacts: [{ vcard }] }}, {quoted: m})
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Masha⁩;;\nFN:Masha\nORG:Masha⁩\nTITLE:\nitem1.TEL;waid=595976230899:5218261000681\nitem1.X-ABLabel:Masha⁩\nX-WA-BIZ-DESCRIPTION:\nX-WA-BIZ-NAME:Masha⁩\nEND:VCARD`
+await conn.sendMessage(m.chat, { contacts: { displayName: 'Masha⁩', contacts: [{ vcard }] }}, {quoted: m})
 }
 handler.help = ['owner']
 handler.tags = ['main']
