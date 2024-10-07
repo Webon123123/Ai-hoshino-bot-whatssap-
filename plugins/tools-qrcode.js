2b507f2083509864672eafa04acbf51d9a55c718
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2024-10-07 14:51:18 -0500

    Update tools-qrcode.js

--- plugins/tools-qrcode.js
+++ plugins/tools-qrcode.js
@@ -3,7 +3,7 @@ import { toDataURL } from 'qrcode'
 let handler = async (m, { conn, text }) => {
 
 if (!text) return conn.reply(m.chat, `🚩 Ingresa un texto junto al comando.`, m)
-conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', listo, m, null, rcanal)
+conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', listo, m, null)
 } 
 handler.help = ['qrcode *<texto>*']
 handler.tags = ['tools']
