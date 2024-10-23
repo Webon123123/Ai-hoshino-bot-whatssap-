24e6a2ec4932cdcbd387a6de72b9a00b8cac9996
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2024-10-23 15:26:13 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -10,43 +10,24 @@ export async function before(m, {conn, participants, groupMetadata}) {
   if (chat.bienvenida && m.messageStubType == 27) {
     let bienvenida = `╔═══✦ ✯ *Ai Hoshino - MD* ✯ ✦═══╗
 
-║ 🎉 ¡Bienvenido! 🎉             
-
-╟─➤ 「 @${m.messageStubParameters[0].split`@`[0]}」    
-
-║  
-
-║ ✨ *Únete a la experiencia exclusiva* ✨
-
-║ en    *Ｓｔａｒｌｉｇｈｔ ｔｅａｍ*
-
+║            
+║                   *𝑻𝒆𝒂𝒎 𝑨𝒔𝒄𝒆𝒏𝒅* *
 ║ 
-
-║ 🔗 *Canal oficial de HoshinoBot*: 
-
-║ [https://whatsapp.com/channel/0029Vak9Hmd1iUxdfDUdCK1w]
-
+║ *🔗 Bienvenido*  
+╟─➤ 「 @${m.messageStubParameters[0].split`@`[0]}」    
+║
 ║  
-
-╚═► 💼 *TK-HOST*:
-
-      🎯 *Adquiere tu propio Bot y usa Ai Hoshino - MD*. 
-
-      💥 *Soporte premium* solo en *TK-HOST*.
-
-🔗 *TK-HOST* (Ventas): 
-
-https://chat.whatsapp.com/FmuetN8qqTx2vEVPmZLc6v`
+╚═►`  
     
-await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, estilo)
+await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, estilo, m, rcanal)
   }
   
   if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+    let bye = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ ¿Te importa? ¡A nosotros no!\n   └───────────────┈ ⳹`
 await conn.sendAi(m.chat, botname, textbot, bye, img, img, estilo)
   }
   
   if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ Jamás te quisimos aquí\n   └───────────────┈ ⳹`
+    let kick = `┌─★ *${botname}* \n│「 ADIOS 👋 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │✑  Se fue\n   │✑ ¿Te importa? ¡A nosotros no!\n   └───────────────┈ ⳹`
 await conn.sendAi(m.chat, botname, textbot, kick, img, img, estilo)
 }}
