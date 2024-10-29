8b8848ebba1b9592e4a6656c3de7f529294c1896
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2024-10-29 15:10:22 -0500

    Update group-banchat.js

--- plugins/group-banchat.js
+++ plugins/group-banchat.js
@@ -1,11 +1,12 @@
 let handler = async (m, { conn, isAdmin, isROwner }) => {
     if (!(isAdmin || isROwner)) return dfail('admin', m, conn)
     global.db.data.chats[m.chat].isBanned = true
-    await conn.reply(m.chat, `🚩 Chat Baneado con exito.`, m)
+    await conn.reply(m.chat, `╭━━━✦❘༻༺❘✦━━━╮\n   🚩 *𝐁𝐨𝐭 𝐀𝐩𝐚𝐠𝐚𝐝𝐨* 🚩\n╰━━━✦❘༻༺❘✦━━━╯\n\nEl bot ha sido *desactivado* en este grupo con éxito.`, m, rcanal)
     await m.react('✅')
 }
+
 handler.help = ['banearbot']
 handler.tags = ['group']
-handler.command = ['banearbot', 'banchat']
+handler.command = ['banearbot', 'botoff', 'offbot' , 'bot-off']
 handler.group = true 
 export default handler
