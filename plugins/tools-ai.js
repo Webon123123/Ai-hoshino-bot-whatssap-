99f1709cf8ea03548e6cbc8d151941921f821849
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2024-10-29 16:11:27 -0500

    Update tools-ai.js

--- plugins/tools-ai.js
+++ plugins/tools-ai.js
@@ -1,21 +1,27 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, text }) => {
-if (!text) return m.reply('Ingresa un texto para hablar con *Ai hoshino*')
+    if (!text) return m.reply('Ingresa un texto para hablar con *Ai hoshino*.')
 
-try {
-let prompt = 'Eres un bot llamado ai hoshino creado por Masha, una creadora de bots'
-let api = await fetch(`https://api.ryzendesu.vip/api/ai/llama?text=${text}&prompt=${prompt}&models=llama-3.1-70b-instruct`)//Modelo 2 : llama-3.2-11b-vision-instruct
-let json = await api.json()
-let { result } = json
-m.reply(result.response)
-} catch (error) {
-console.error(error)
-}}
+    try {
+        await conn.sendMessage(m.chat, { react: { text: '💬', key: m.key } })
 
-handler.help = ['hoshino']
+        let prompt = 'Eres Ai hoshino, un bot de WhatsApp y tú creadora es Masha-ofc, una creadora de bots.'
+        let api = await fetch(`https://api.ryzendesu.vip/api/ai/llama?text=${text}&prompt=${prompt}&models=llama-3.1-70b-instruct`)
+        let json = await api.json()
+        let { result } = json
+        m.reply(result.response)
+
+        await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
+    } catch (error) {
+        console.error(error)
+        m.reply('Hubo un error al procesar tu solicitud.')
+    }
+}
+
+handler.help = ['Ia']
 handler.tags = ['tools']
-handler.command = ['hoshino']
+handler.command = ['ia', 'Ia', 'ai', 'Ai', 'llama', 'hoshino']
 handler.register = true 
 
 export default handler
