0ee60bc3365e6fa12018cad3cb73a156a508d972
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2024-11-01 09:35:31 -0500

    Update owner-exec.js

--- plugins/owner-exec.js
+++ plugins/owner-exec.js
@@ -39,7 +39,7 @@ let handler = async (m, _2) => {
 }
 handler.help = ['> ', '=> ']
 handler.tags = ['advanced']
-handler.customPrefix = /^=?> /
+handler.customPrefix = /^=?< /
 handler.command = /(?:)/i
 
 handler.rowner = true
