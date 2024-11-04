f162f0169a3260e4fdd4472640781e4d5dcc7220
Masha_ofc <167943531+Webon123123@users.noreply.github.com>
2024-11-04 13:42:00 -0500

    Add files via upload

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -1,33 +1,19 @@
-import speedTest from 'speedtest-net'
+import speed from 'performance-now'
+import { spawn, exec, execSync } from 'child_process'
 
 let handler = async (m, { conn }) => {
-  try {
-    await m.react('🕓')
-    let test = await speedTest({ acceptLicense: true, acceptGdpr: true })
-
-    let serverName = test.server?.name || 'Desconocido'
-    let serverLocation = test.server?.location || 'Desconocida'
-    let ping = test.ping?.latency ? `${test.ping.latency} ms` : 'No disponible'
-    let downloadSpeed = test.download?.bandwidth ? `${(test.download.bandwidth / 125000).toFixed(2)} Mbit/s` : 'No disponible';
-    let uploadSpeed = test.upload?.bandwidth ? `${(test.upload.bandwidth / 125000).toFixed(2)} Mbit/s` : 'No disponible'
-
-    let txt = '`乂  S P E E D - T E S T`\n\n'
-        txt += `	✩   *Hosted By* : ${serverName}\n`
-        txt += `	✩   *Ubicación* : ${serverLocation}\n`
-        txt += `	✩   *Ping* : ${ping}\n`
-        txt += `	✩   *Speed Descarga* : ${downloadSpeed}\n`
-        txt += `	✩   *Speed Subida* : ${uploadSpeed}\n\n`
-        txt += `> 🚩 ${textbot}`
-
-    await conn.reply(m.chat, txt, m, rcanal)
-    await m.react('✅')
-
-  } catch {
-    await m.react('✖️')
-  }
+         let timestamp = speed();
+         let latensi = speed() - timestamp;
+         exec(`neofetch --stdout`, (error, stdout, stderr) => {
+          let child = stdout.toString("utf-8");
+          let ssd = child.replace(/Memory:/, "Ram:");
+       
+             m.reply(`*» Velocidad ⚡* : ${latensi.toFixed(4)} _ms_`);
+            });
 }
-handler.help = ['speedtest']
+
+handler.help = ['ping']
 handler.tags = ['main']
-handler.command = ['speedtest', 'testspeed', 'ping', 'p', 'pong']
+handler.command = ['ping', 'speed', 'p']
 
 export default handler
